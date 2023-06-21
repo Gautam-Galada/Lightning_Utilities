c6e18c0ff4f3599c33f0589b42d08514e1d604bf
Anorak <65299277+anorak-7@users.noreply.github.com>
2023-06-21 11:24:34 +0530

    Create lightningmodule.py

---
+++ lightningmodule.py
@@ -0,0 +1,51 @@
+class LightningModel(L.LightningModule):
+    def __init__(self, model, learning_rate):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.model = model
+        self.save_hyperparameters(ignore=["model"])
+        self.train_acc = torchmetrics.Accuracy(task="multiclass", num_classes=5)
+        self.val_acc = torchmetrics.Accuracy(task="multiclass", num_classes=5)
+        self.test_acc = torchmetrics.Accuracy(task="multiclass", num_classes=5)
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def _shared_step(self, batch):
+        features, true_labels = batch
+        logits = self(features)
+        loss = F.cross_entropy(logits, true_labels)
+        predicted_labels = torch.argmax(logits, dim=1)
+        return loss, true_labels, predicted_labels
+
+    def training_step(self, batch, batch_idx):
+        loss, true_labels, predicted_labels = self._shared_step(batch)
+        self.log("train_loss", loss)
+        self.train_acc(predicted_labels, true_labels)
+        self.log("train_acc", self.train_acc, prog_bar=True, on_epoch=True, on_step=False)
+        
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, true_labels, predicted_labels = self._shared_step(batch)
+        self.log("val_loss", loss, prog_bar=True)
+        self.val_acc(predicted_labels, true_labels)
+        self.log("val_acc", self.val_acc, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        _, true_labels, predicted_labels = self._shared_step(batch)
+        self.test_acc(predicted_labels, true_labels)
+        self.log("test_acc", self.test_acc)
+
+    def configure_optimizers(self):
+        opt = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
+        sch = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, mode='min', factor=0.1, patience=5)
+        return {
+            "optimizer": opt,
+            "lr_scheduler": {
+                "scheduler": sch,
+                "monitor": "train_loss",
+                "interval": "epoch", # default
+                "frequency": 1, # default
+            },
+        }
